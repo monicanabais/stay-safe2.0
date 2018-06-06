5c03872d0af47a7938a7144466538ef4a408e32e
Maria Castro <mariacantocastro@gmail.com>
2018-06-06 16:23:22 +0100

    admin dashboard done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_06_120317) do
+ActiveRecord::Schema.define(version: 2018_06_06_151128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 2018_06_06_120317) do
     t.float "latitude"
     t.float "longitude"
     t.string "location"
-    t.string "type"
+    t.string "kind"
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2018_06_06_120317) do
     t.string "last_name"
     t.string "avatar"
     t.integer "state", default: 0, null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
