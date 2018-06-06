98f99fb91c038066b6baed5b7c4451209c07bd7f
Monica Nabais <monicamnabais@gmail.com>
2018-06-06 12:14:47 +0100

    Create and update devise

---
+++ db/migrate/20180606111040_add_fields_to_devise.rb
@@ -0,0 +1,8 @@
+class AddFieldsToDevise < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :avatar, :string
+    add_column :users, :state, :integer, null: false, default: 0
+  end
+end
