ec971cc041f5893911f74d767dd79e740375ed5c
Maria Castro <mariacantocastro@gmail.com>
2018-06-07 15:03:28 +0100

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "users#index"
+
   resources :users do
     resources :friend_requests, only: [:create]
   end
 
   resources :friend_requests, except: [:show, :create]
-  root to: "users#index" 
+  root to: "users#show/:id"
   resources :hazard_notifications, only: :index
   resources :friendships, only: [:index, :update, :destroy]
   resources :current_locations, only: [:new, :create, :destroy]
