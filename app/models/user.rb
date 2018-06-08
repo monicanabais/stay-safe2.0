de68925e2e679fba516acad435f8438d3876c145
Maria Castro <mariacantocastro@gmail.com>
2018-06-08 10:53:24 +0100

    comment things

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,26 +19,26 @@ class User < ApplicationRecord
   include PgSearch
 
   pg_search_scope :search_for, against: [:first_name, :last_name, :email],
- using: { tsearch: { any_word: true, dictionary: :english, prefix: true}, :trigram => { :threshold => 0.1 }  },
- :associated_against => {
-          comments: [:message],
-          user: [:first_name, :last_name, :email]}
+  using: { tsearch: { any_word: true, dictionary: :english, prefix: true}, :trigram => { :threshold => 0.1 }  },
+  :associated_against => {
+    comments: [:message],
+    user: [:first_name, :last_name, :email]}
 
 
-  def remove_friend(friend)
-    self.friends.destroy(friend)
-  end
+    def remove_friend(friend)
+      self.friends.destroy(friend)
+    end
 
-  def friends_notified
-    self.friendships._select! do |friend|
-      HazardNotification.where(user: friend).map(&:user).include? self.friendships
+    def friends_notified
+      self.friendships._select! do |friend|
+        HazardNotification.where(user: friend).map(&:user).include? self.friendships
+      end
+    end
+
+    def friendship_with(friend)
+      Friendship.where(user:self, friend:friend).first
     end
   end
-
-  def friendship_with(friend)
-    Friendship.where(user:self, friend:friend).first
-  end
-end
 
 
 
