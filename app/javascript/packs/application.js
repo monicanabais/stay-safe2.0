9d092a58465063e24fa061045942587e32d8dcf0
Monica Nabais <monicamnabais@gmail.com>
2018-06-09 19:29:16 +0100

    complete search and cards, automate alert messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,3 +21,9 @@ document.app = {
 
  import { rotateCards } from '../components/cards';
 rotateCards();
+
+setTimeout(fade_out, 2000);
+
+function fade_out() {
+  $(".alert-dismissible").fadeOut().empty();
+}
