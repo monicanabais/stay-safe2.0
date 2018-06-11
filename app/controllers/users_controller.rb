d9d3e046b661874d5cd436929d524a1fedfbb627
Maria Castro <mariacantocastro@gmail.com>
2018-06-11 18:41:30 +0100

    bug fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     @friends = current_user.friends
     if params[:query].present?
       sql_query = "first_name ILIKE :query OR last_name ILIKE :query"
-      @users = User.where(sql_query, query: "%#{params[:query]}%")
+      @users = User.where(sql_query, query: "%#{params[:query]}%") - @friends - [current_user]
     elsif params[:query] && params[:query].empty?
       @users = User.all - @friends - [current_user]
     else
