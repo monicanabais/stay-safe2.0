1aa46e7265433640fc050dde7244d3234656a3fc
Monica Nabais <monicamnabais@gmail.com>
2018-06-14 19:07:40 +0100

    minor changes on search

--- app/models/hazard.rb
+++ app/models/hazard.rb
@@ -1,5 +1,5 @@
 class Hazard < ApplicationRecord
-  has_many :hazard_notifications
+  has_many :hazard_notifications, dependent: :destroy
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
   validates :kind, acceptance: { accept: ['Forest fire', 'Building fire'] }
